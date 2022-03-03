eaa19eb7c543e38d2b3df9524e90b3ccec54cdd5
Lucas Melo <lucasnmelo321@gmail.com>
2022-03-02 23:01:54 -0300

    Desafio 3

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { rawListeners } from "process";
 
 dotenv.config();
 
@@ -38,6 +39,57 @@ app.listen(PORT, () => {
 
 
 	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
-	// R01, R02, R03, R04, R05
+	//								 1  2  3  4  5 
+	// 	let  notas = [10, 8, 6, 5, 3]
+	// //									 1        2         3       4        5 
+	// 	const alunos = ['Lucas', 'Matheus', 'Luis', 'Luan', 'Lino']
+		 
+	// 	if(notas = 10) {
+	// 			console.log('passou')
+	// 	} else if(notas <=9){
+	// 		console.log('reprovado')
+	// 	}
+
+	const readline = require ('readline');
+	const rl = readline.createInterface ({
+		input: process.stdin,
+		output: process. stdout
+	});
+
 	
-});
+	
+
+
+		//R01
+	rl.question('Quantos alunos tem? \n', (name: any) => {
+		//R02
+		console.log('A quantidade de alunos é: ' + name);
+    //R03
+		rl.question(`Qual o nome do aluno(a)? \n`, (name : any) =>{
+			console.log(`Ola ${name}`);
+		rl.question(`Qual a nota do ${name}? \n`, (nota : number) => {
+			console.log(`A nota do(a) ${name} é: ${nota}`)
+	
+	rl.question(`Qual o nome do aluno(a)? \n`, (name2 : any) =>{
+			console.log(`Ola ${name2}`);
+		rl.question(`Qual a nota do ${name2}? \n`, (nota2 : number) => {
+			console.log(`A nota do(a) ${name2} é: ${nota2}`)
+	
+	
+	rl.question(`Qual o nome do aluno(a)? \n`, (name3 : any) =>{
+		console.log(`Ola ${name3}`);
+	rl.question(`Qual a nota do ${name3}? \n`, (nota3 : number) => {
+		console.log(`A nota do(a) ${name3} é: ${nota3}`)
+	
+	
+		//#R04
+		console.log('A maior nota é:' + Math.max(nota, nota2, nota3));
+	
+	});
+	});
+	});
+	});
+	});
+	});
+	});
+	});
